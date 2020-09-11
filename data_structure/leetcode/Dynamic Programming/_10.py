a36169b92d6a22f4e19b1a3dd3dc1b2e4a16f530
TobeyLi <1147413415@qq.com>
2020-09-11 09:59:34 +0800

    update 10 .Regular Expression Matching

--- data_structure/leetcode/Dynamic Programming/_10.py
+++ data_structure/leetcode/Dynamic Programming/_10.py
@@ -31,11 +31,11 @@ class Solution:
             return len(s) == 0
         if len(p) == 1:
             return len(s) == 1 and (s[0] == p[0] or p[0] == ".")
-        if p[2] != "*":
+        if p[1] != "*":
             if len(s) == 0:
                 return False
             return (s[0] == p[0] or p[0] == '.') and self.isMatch(s[1:], p[1:])
-        # p[2] = "*" 的情况
+        # p[1] = "*" 的情况
         while len(s) != 0 and (s[0] == p[0] or p[0] == '.'):
             if self.isMatch(s, p[2:]):
                 return True

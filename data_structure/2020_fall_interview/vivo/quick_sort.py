77b8379ac4e66fc661866098df77e7f7d9fb027d
TobeyLi <1147413415@qq.com>
2020-09-17 09:24:01 +0800

    add quickSort non recursion version

--- data_structure/2020_fall_interview/vivo/quick_sort.py
+++ data_structure/2020_fall_interview/vivo/quick_sort.py
@@ -1,12 +1,49 @@
-
-
-def quick_sort(array,low,high):
+def quickSort(array, low, high):
+    """
+    recursion version
+    :param array:
+    :param low:
+    :param high:
+    :return:
+    """
     if low < high:
-        key_index = partion(array,low,high)
-        quick_sort(array,low,key_index-1)
-        quick_sort(array,key_index+1,high)
+        keyIndex = partion(array, low, high)
+        quickSort(array, low, keyIndex - 1)
+        quickSort(array, keyIndex + 1, high)
 
-def partion(array,low,high):
+
+def quickSortNonRecursion(array, low, high):
+    """
+    non recursion version
+    :param array:
+    :param low:
+    :param high:
+    :return:
+    """
+    stack = []
+    if low < high:
+        mid = partion(array, low, high)
+        if low < mid - 1:
+            stack.append(low)
+            stack.append(mid - 1)
+        if mid + 1 < high:
+            stack.append(mid + 1)
+            stack.append(high)
+        while len(stack) != 0:
+            q = stack[-1]
+            stack.pop(-1)
+            p = stack[-1]
+            stack.pop(-1)
+            mid = partion(array, p, q)
+            if p < mid - 1:
+                stack.append(p)
+                stack.append(mid - 1)
+            if mid + 1 < q:
+                stack.append(mid + 1)
+                stack.append(q)
+
+
+def partion(array, low, high):
     key = array[low]
     while low < high:
         while low < high and array[high] >= key:
@@ -20,7 +57,8 @@ def partion(array,low,high):
     array[low] = key
     return low
 
+
 if __name__ == '__main__':
-    nums=[4,2,5,7,3,4]
-    quick_sort(nums,0,len(nums)-1)
+    nums = [4, 2, 5, 7, 3, 4]
+    quickSortNonRecursion(nums, 0, len(nums) - 1)
     print(nums)
